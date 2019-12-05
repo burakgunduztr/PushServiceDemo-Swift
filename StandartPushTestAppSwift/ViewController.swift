46d202cea16a85c3121504ee158ca99a84677f24
burakgunduztr <burakgunduz@outlook.com.tr>
2019-12-05 11:30:18 +0300

    Initial Commit

---
+++ StandartPushTestAppSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StandartPushTestAppSwift
+//
+//  Created by Burak Gunduz on 5.12.2019.
+//  Copyright © 2019 StandartPushTestAppSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
