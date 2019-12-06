d83087dac69b68aa73c71600d24a47732f8b7a0a
burakgunduztr <burakgunduz@outlook.com.tr>
2019-12-06 21:18:38 +0300

    updated data with notification cat.

--- StandartPushTestAppSwift/AppDelegate.swift
+++ StandartPushTestAppSwift/AppDelegate.swift
@@ -20,24 +20,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         let center = UNUserNotificationCenter.current()
         center.delegate = self
         
-        let acceptAction = UNNotificationAction(identifier: "ACCEPT_ACTION",
-                                                title: "Accept",
-                                                options: UNNotificationActionOptions(rawValue: 0))
-        let declineAction = UNNotificationAction(identifier: "DECLINE_ACTION",
-                                                 title: "Decline",
-                                                 options: UNNotificationActionOptions(rawValue: 0))
-        // Define the notification type
-        let meetingInviteCategory =
-            UNNotificationCategory(identifier: "MEETING_INVITATION",
-                                   actions: [acceptAction, declineAction],
-                                   intentIdentifiers: [],
-                                   hiddenPreviewsBodyPlaceholder: "",
-                                   options: .customDismissAction)
- 
-        center.setNotificationCategories([meetingInviteCategory])
-        
-        center.requestAuthorization(options: [.alert, .sound]) { (status: Bool, error: Error?) in
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { (status: Bool, error: Error?) in
             if error == nil {
+                self.registerCategory()
+                // Now, I can see action buttons for that scheduled notification, but not from payload (nodeJS)
+//                self.scheduleNotification(event: "test", interval: 3)
                 DispatchQueue.main.async {
                     UIApplication.shared.registerForRemoteNotifications()
                 }
@@ -52,6 +39,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         return true
     }
     
+    func registerCategory() -> Void {
+        
+        let acceptAction = UNNotificationAction(identifier: "ACCEPT_ACTION",
+                                                title: "Accept",
+                                                options: UNNotificationActionOptions(rawValue: 0))
+        let declineAction = UNNotificationAction(identifier: "DECLINE_ACTION",
+                                                 title: "Decline",
+                                                 options: UNNotificationActionOptions(rawValue: 0))
+        
+        let category : UNNotificationCategory = UNNotificationCategory.init(identifier: "MEETING_INVITATION", actions: [acceptAction, declineAction], intentIdentifiers: [], options: [])
+        
+        let center = UNUserNotificationCenter.current()
+        center.setNotificationCategories([category])
+        
+    }
+    
+    func scheduleNotification (event : String, interval: TimeInterval) {
+        let content = UNMutableNotificationContent()
+        
+        content.title = event
+        content.body = "body"
+        content.categoryIdentifier = "MEETING_INVITATION"
+        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: interval, repeats: false)
+        let identifier = "id_" + event
+        let request = UNNotificationRequest.init(identifier: identifier, content: content, trigger: trigger)
+        
+        let center = UNUserNotificationCenter.current()
+        center.add(request, withCompletionHandler: { (error) in
+        })
+    }
+    
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
         print("Handled deviceToken: ")
@@ -60,87 +78,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         
-        // Get the meeting ID from the original notification.
-        let userInfo = response.notification.request.content.userInfo
-        
         // Perform the task associated with the action.
         switch response.actionIdentifier {
         case "ACCEPT_ACTION":
-            
             break
-            
         case "DECLINE_ACTION":
-            
             break
-            
-            // Handle other actions…
-            
         default:
             break
         }
         
-        // Always call the completion handler when done.
         completionHandler()
     }
     
-//    // Modify the payload contents.
-//    func didReceive(_ request: UNNotificationRequest,
-//                             withContentHandler contentHandler:
-//        @escaping (UNNotificationContent) -> Void) {
-//        self.contentHandler = contentHandler
-//        self.bestAttemptContent = (request.content.mutableCopy()
-//            as? UNMutableNotificationContent)
-//
-//        // Try to decode the encrypted message data.
-//        let encryptedData = bestAttemptContent?.userInfo["ENCRYPTED_DATA"]
-//        if let bestAttemptContent = bestAttemptContent {
-//            if let data = encryptedData as? String {
-//                let decryptedMessage = self.decrypt(data: data)
-//                bestAttemptContent.body = decryptedMessage
-//            }
-//            else {
-//                bestAttemptContent.body = "(Encrypted)"
-//            }
-//
-//            // Always call the completion handler when done.
-//            contentHandler(bestAttemptContent)
-//        }
-//    }
-//
-//    // Return something before time expires.
-//    func serviceExtensionTimeWillExpire() {
-//        if let contentHandler = contentHandler,
-//            let bestAttemptContent = bestAttemptContent {
-//
-//            // Mark the message as still encrypted.
-//            bestAttemptContent.subtitle = "(Encrypted)"
-//            bestAttemptContent.body = ""
-//            contentHandler(bestAttemptContent)
-//        }
-//    }
-    
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler(.alert)
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
