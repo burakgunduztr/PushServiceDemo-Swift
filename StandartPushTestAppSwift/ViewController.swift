686a8e35c6609f784e029b71a44bbe0eb2c0946f
burakgunduztr <burakgunduz@outlook.com.tr>
2020-01-22 16:03:00 +0300

    getDeliveredNotifications logs are added

--- StandartPushTestAppSwift/ViewController.swift
+++ StandartPushTestAppSwift/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("notifications are fetched:")
+        
+        let center = UNUserNotificationCenter.current()
+        center.getDeliveredNotifications { (notifications: [UNNotification]) in
+            print(notifications.description)
+        }
     }
 }
 
